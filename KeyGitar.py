89e31c6dd0308e779999b3790bed5f2abaca1bdb
00MY00 <75865145+00MY00@users.noreply.github.com>
2023-02-07 23:46:54 +0100

    Add files via upload

---
+++ KeyGitar.py
@@ -0,0 +1,57 @@
+########################################
+#     Script crée par Kuroakashiro     #
+########################################
+# Importation des Libreries
+print("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+try:
+    import pygame
+    print("Pygame est installé et importé correctement.")
+except ImportError:
+    print("Pygame n'est pas installé ou n'a pas été importé correctement.")
+try:
+    import keyboard
+    print("keyboard est installé et importé correctement.")
+except ImportError:
+    print("keyboard n'est pas installé ou n'a pas été importé correctement.")
+try:
+    import os
+    print("os est installé et importé correctement.")
+except ImportError:
+    print("os n'est pas installé ou n'a pas été importé correctement.")
+try:
+    from pydub import AudioSegment
+    import simpleaudio as sa
+    print("pydub est installé et importé correctement.")
+except ImportError:
+    print("pydub n'est pas installé ou n'a pas été importé correctement.")
+print("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+
+
+
+
+
+
+
+
+
+
+
+
+###
+# Variable Racine
+
+
+###
+# Convertion des fichier audio en 'WAV'
+
+
+###
+# Variable de fichier audio
+
+###
+
+################################################################
+
+
+
+
