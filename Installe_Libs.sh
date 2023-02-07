13978ace6f69e2dd0d05059dc11aff6a4875469f
00MY00 <75865145+00MY00@users.noreply.github.com>
2023-02-07 11:42:50 +0100

    Add files via upload

---
+++ Installe_Libs.sh
@@ -0,0 +1,10 @@
+apt update -y && apt upgrade -y
+python get-pip.py
+python -m ensurepip --upgrade
+pip install keyboard
+# Temporaire
+pip install pygame --pre
+pip install pydub
+pip install ffmpeg --use-pep517
+
+
