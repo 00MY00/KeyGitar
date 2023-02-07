35da606f46cf9731cee29ade6280a6b1ac1d9251
00MY00 <75865145+00MY00@users.noreply.github.com>
2023-02-07 11:50:25 +0100

    Add files via upload

--- Installe_Libs.sh
+++ Installe_Libs.sh
@@ -6,7 +6,11 @@ pip install keyboard
 pip install pygame --pre
 pip install pydub
 pip install ffmpeg --use-pep517
-
+apt install ffmpeg -y
 if [ $? -eq 0 ];
 then
     echo -e "\033[32m[ OK ] Installation términée !\033[00m"
+
+
+python3 PlaySond_PyDub.py
+
